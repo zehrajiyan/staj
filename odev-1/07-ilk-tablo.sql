e2ce5b8046aa87cf736f9a42f0aec4ed3cd9f0f2
zehrajiyan <zehra.jiyan@hotmail.com>
2026-08-01 23:33:38 +0300

    sql dosyasının guncellenmesi

---
+++ odev-1/07-ilk-tablo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tablo_adi (
+    kolon1_adi veri_tipi kisitlamalar,
+    kolon2_adi veri_tipi kisitlamalar,
+    kolon3_adi veri_tipi
+);
+
+INSERT INTO tablo_adi (kolon1, kolon2, kolon3) 
+VALUES (deger1, deger2, deger3),
+       (deger4, deger5, deger6),
+       (deger7, deger8, deger9);
